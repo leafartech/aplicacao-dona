1619c56cb4e338c98062ca68d11e9c517ca7dbbd
leafartech <119262053+leafartech@users.noreply.github.com>
2024-11-03 10:03:37 -0300

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 import "./globals.css";
 
-const poppins = Poppins({ weight: [ '400', '600', '700' ], subsets: ['latin'] })
+const poppins = Poppins({ weight: ['400', '600', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Aplicação - Dona do Plantão",
@@ -16,9 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MLKKLHK')
+        `}} />
+      </head>
       <body
         className={`${poppins.className}`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLKKLHK"
+          height="0" width="0"></iframe></noscript>
         {children}
       </body>
     </html>
