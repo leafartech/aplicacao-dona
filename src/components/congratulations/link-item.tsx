c471bce89b611b8e2d6636f4f5023ac341cb6a03
Kibryant <arthurgustavon@gmail.com>
2025-01-13 16:21:27 -0300

    fix bug

--- src/components/congratulations/link-item.tsx
+++ src/components/congratulations/link-item.tsx
@@ -14,7 +14,7 @@ export function LinkItem({ icon, alt, href, label }: LinkItemProps) {
 				href={href}
 				target="_blank"
 				rel="noopener noreferrer"
-				className="bg-[#4fcb5c] shadow-xl w-full max-w-sm flex items-center justify-center gap-2 px-8 text-center py-2 font-semibold rounded-xl text-lg sm:text-xl text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-[#c68a1a] transition-all"
+				className="bg-[#4fcb5c] shadow-md w-full max-w-sm flex items-center justify-center gap-2 px-8 text-center py-2 font-semibold rounded-xl text-lg sm:text-xl text-white focus:ring-2 focus:ring-offset-2 focus:ring-[#c68a1a] transition-all"
 			>
 				<div className="relative w-[26px] h-[26px]">
 					<Image
