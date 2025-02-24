06a130fa900c1b3d73147273ee2cc44382762436
Rafael Borges Bezerra <leafartech@gmail.com>
2025-02-24 12:41:26 -0300

    updt respondi addeed

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,6 +8,16 @@ interface ButtonProps {
 }
 
 export default function Button({ label, open, setOpen }: ButtonProps) {
+
+	return (
+		<a
+			className="z-[999] w-full max-w-md text-center bg-gradient-to-br from-primary-light to-primary-dark hover:to-primary hover:from-primary py-2 rounded-md text-white sm:text-xl font-semibold uppercase shadow-2xl shadow-primary-light focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+			href="https://form.respondi.app/qiz1HBoA"
+		>
+			{label}
+		</a>
+	)
+
 	return (
 		<button
 			type="button"
